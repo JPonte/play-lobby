4a7dfb9c2bdc5868ace8622c37ba8e86e5ebd508
JPonte <joaopcponte@gmail.com>
2020-06-04 00:33:50 +0200

    Basic game creation

--- server/app/controllers/LobbyController.scala
+++ server/app/controllers/LobbyController.scala
@@ -10,16 +10,22 @@ import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents, W
 import scala.concurrent.{ExecutionContext, Future}
 import akka.pattern.ask
 import akka.util.Timeout
-import core.Username
+import core.{GameInfo, Username}
+import models.GameInfoRepository
+import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
+import slick.jdbc.JdbcProfile
 
 import scala.concurrent.duration._
 
 @Singleton
 class LobbyController @Inject()(val controllerComponents: ControllerComponents,
-                                userAction: UserAction
-                               )(implicit system: ActorSystem, executionContext: ExecutionContext) extends BaseController {
+                                userAction: UserAction,
+                                val dbConfigProvider: DatabaseConfigProvider
+                               )(implicit system: ActorSystem, executionContext: ExecutionContext)
+  extends BaseController with HasDatabaseConfigProvider[JdbcProfile] {
 
   private val lobbyManager = system.actorOf(Props[LobbyManager], "LobbyManager")
+  private val gameRepository = new GameInfoRepository(db)
 
   def index(): Action[AnyContent] = userAction.async { implicit request: UserRequest[AnyContent] =>
     implicit val timeout: Timeout = Timeout(5.seconds)
@@ -50,10 +56,35 @@ class LobbyController @Inject()(val controllerComponents: ControllerComponents,
     }
   }
 
-  def partyLobby(): Action[AnyContent] = userAction { implicit request: UserRequest[AnyContent] =>
-    request.username.fold(Redirect(routes.LoginController.login())) { username =>
-      val webSocketUrl = routes.LobbyController.socket().webSocketURL()
-      Ok(views.html.party_lobby(webSocketUrl, username.value, Seq(username.value, "ponte", "merdas")))
+  def partyLobby(): Action[AnyContent] = userAction.async { implicit request: UserRequest[AnyContent] =>
+    request.username.fold(Future.successful(Redirect(routes.LoginController.login()))) { username =>
+      gameRepository.getWaitingGamesForUser(username).map(_.headOption).map {
+        case Some(GameInfo(gameId, _, _, players, _)) =>
+          val webSocketUrl = routes.LobbyController.socket().webSocketURL()
+          Ok(views.html.party_lobby(webSocketUrl, username.value, gameId, players.map(_.value)))
+        case _ =>
+          Redirect(routes.LobbyController.index())
+      }
+    }
+  }
+
+  def createGame(): Action[AnyContent] = userAction.async { implicit request: UserRequest[AnyContent] =>
+    request.username.fold(Future.successful(Redirect(routes.LoginController.login()))) { username =>
+      val pw = (for {
+        body <- request.body.asFormUrlEncoded
+        password <- body.get("password").flatMap(_.headOption)
+      } yield password.trim)
+        .filter(_.nonEmpty)
+
+      gameRepository.createGame(2, pw).flatMap {
+        case Some(gameInfo) =>
+          gameRepository.joinGame(username, gameInfo.gameId).map {
+            case true => Redirect(routes.LobbyController.partyLobby())
+            case _ => Redirect(routes.LobbyController.index()).flashing("error" -> "Failed to join the game")
+          }
+        case _ =>
+          Future.successful(Redirect(routes.LobbyController.index()).flashing("error" -> "Failed to create a game"))
+      }
     }
   }
 
--- server/app/models/GameInfoRepository.scala
+++ server/app/models/GameInfoRepository.scala
@@ -33,18 +33,31 @@ class GameInfoRepository(db: Database)(implicit val executionContext: ExecutionC
 
   def getGameInfo(gameId: Int): Future[Option[GameInfo]] = {
     val dbQuery = (for {
-      game <- Tables.GameInfo if game.id === gameId
-      gamePlayers <- Tables.GamePlayers.filter(_.gameId === game.id)
-    } yield (game, gamePlayers)).result
-    db.run(dbQuery).map(_.groupBy(_._1).headOption.map(r => dbToGameInfo(r._1, r._2.map(_._2))))
+      game <- Tables.GameInfo.filter(_.id === gameId) joinLeft Tables.GamePlayers on (_.id === _.gameId)
+    } yield (game._1, game._2)).result
+    db.run(dbQuery).map(_.groupBy(_._1).headOption.map(r => dbToGameInfo(r._1, r._2.flatMap(_._2))))
+  }
+
+  def getAllRunningGames: Future[immutable.Iterable[GameInfo]] = {
+    val dbQuery = (for {
+      game <- Tables.GameInfo.filter(_.status === GameStatus.Running) joinLeft Tables.GamePlayers on (_.id === _.gameId)
+    } yield (game._1, game._2)).result
+    db.run(dbQuery).map(_.groupBy(_._1).map(r => dbToGameInfo(r._1, r._2.flatMap(_._2))))
+  }
+
+  def getAllWaitingGames: Future[immutable.Iterable[GameInfo]] = {
+    val dbQuery = (for {
+      game <- Tables.GameInfo.filter(_.status === GameStatus.WaitingToStart) joinLeft Tables.GamePlayers on (_.id === _.gameId)
+    } yield (game._1, game._2)).result
+    db.run(dbQuery).map(_.groupBy(_._1).map(r => dbToGameInfo(r._1, r._2.flatMap(_._2))))
   }
 
   def joinGame(username: Username, gameId: Int): Future[Boolean] = {
     getGameInfo(gameId).map(_.exists { gameInfo =>
-      gameInfo.status == GameStatus.WaitingToStart && gameInfo.playerCount > gameInfo.players.size
+      gameInfo.status == GameStatus.WaitingToStart && gameInfo.playerCount > gameInfo.players.size && !gameInfo.players.contains(username)
     }).flatMap {
       case true =>
-        db.run(Tables.GamePlayers.insertOrUpdate(GamePlayersRow(gameId, username.value))).map(_ > 0)
+        db.run(Tables.GamePlayers += GamePlayersRow(gameId, username.value)).map(_ > 0)
       case _ =>
         Future.successful(false)
     }
